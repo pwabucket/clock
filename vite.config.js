63943233cc3c2b87b67a4f3678a8bc312de88cb4
Sadiq Salau <sadiqsalau888@gmail.com>
2023-08-07 07:57:27 +0100

    Configured vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,54 @@
+import process from "process";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
+import { loadEnv } from "vite";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve("src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
     },
-  },
-  plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        manifest: {
+          name: env.VITE_PWA_MANIFEST_NAME,
+          short_name: env.VITE_PWA_MANIFEST_SHORT_NAME,
+          description: env.VITE_PWA_MANIFEST_DESCRIPTION,
+          theme_color: env.VITE_PWA_MANIFEST_THEME_COLOR,
+          background_color: env.VITE_PWA_MANIFEST_BACKGROUND_COLOR,
+          icons: [
+            {
+              src: "pwa-64x64.png",
+              sizes: "64x64",
+              type: "image/png",
+            },
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
+            },
+            {
+              src: "maskable-icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
