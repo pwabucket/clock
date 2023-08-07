196a382c5b12655fe8ce4e2a4b93e14a8c318d7c
Sadiq Salau <sadiqsalau888@gmail.com>
2023-08-07 03:44:28 +0100

    Render react-clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
-export const App = () => null;
+import Clock from "react-clock";
+import clsx from "clsx";
+import { useInterval } from "react-use";
+import { useState } from "react";
+
+export const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  // Update time
+  useInterval(() => setTime(new Date()), 1000);
+
+  return (
+    <div
+      className={clsx(
+        "container p-4 mx-auto",
+        "flex justify-center items-center",
+        "min-h-screen supports-[min-height:100dvh]:min-h-[100dvh]"
+      )}
+    >
+      <div className="w-full max-w-xs">
+        <Clock
+          value={time}
+          size="100%"
+          className="aspect-square"
+          renderNumbers
+        />
+      </div>
+    </div>
+  );
+};
